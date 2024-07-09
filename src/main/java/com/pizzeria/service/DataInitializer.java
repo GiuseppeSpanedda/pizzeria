f666d46ae04e1359b948e45aa7c70d14b51b3db9
Giuseppe Spanedda <gspanedda@athena.it>
2024-07-09 14:11:48 +0200

    aggiustamenti

--- src/main/java/com/pizzeria/service/DataInitializer.java
+++ src/main/java/com/pizzeria/service/DataInitializer.java
@@ -14,18 +14,18 @@ public class DataInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         // Esegui gli script SQL per popolare le tabelle
-        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('mozzarella', '1.5')");
-        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('pomodoro', '1')");
-        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('patatine', '1.5')");
-        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('salame', '2')");
-        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('melanzane', '1.20')");
-        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('zucchine', '1.30')");
-        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('peperoni', '1')");
-        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('cipolle', '1')");
+        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('Mozzarella', '1.5')");
+        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('Pomodoro', '1')");
+        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('Patatine', '1.5')");
+        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('Salame', '2')");
+        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('Melanzane', '1.20')");
+        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('Zucchine', '1.30')");
+        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('Peperoni', '1')");
+        jdbcTemplate.execute("INSERT INTO INGREDIENTI  (nome, price) VALUES ('Cipolle', '1')");
 
-        jdbcTemplate.execute("INSERT INTO PIZZE (name, price) VALUES ('vegetariana', '7')");
-        jdbcTemplate.execute("INSERT INTO PIZZE (name, price) VALUES ('diavola', '4.5')");
-        jdbcTemplate.execute("INSERT INTO PIZZE (name, price) VALUES ('americana', '4')");
+        jdbcTemplate.execute("INSERT INTO PIZZE (name, price) VALUES ('Vegetariana', '7')");
+        jdbcTemplate.execute("INSERT INTO PIZZE (name, price) VALUES ('Diavola', '4.5')");
+        jdbcTemplate.execute("INSERT INTO PIZZE (name, price) VALUES ('Americana', '4')");
 
         jdbcTemplate.execute("INSERT INTO PIZZA_INGREDIENTI (PIZZA_ID, INGREDIENTI_ID) VALUES ('1', '1')");
         jdbcTemplate.execute("INSERT INTO PIZZA_INGREDIENTI (PIZZA_ID, INGREDIENTI_ID) VALUES ('1', '2')");
