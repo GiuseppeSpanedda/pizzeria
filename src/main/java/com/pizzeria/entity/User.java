6dbfff1294a0bb43efd7ca35965146e7180f5c7a
Srdn <giuseppespanedda@outlook.it>
2024-11-21 22:54:39 +0100

    Implementing Spring Security

---
+++ src/main/java/com/pizzeria/entity/User.java
@@ -0,0 +1,21 @@
+package com.pizzeria.entity;
+
+import lombok.Data;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role;
+
+}
+
---
+++ src/main/java/com/pizzeria/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pizzeria.repository;
+
+import com.pizzeria.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
