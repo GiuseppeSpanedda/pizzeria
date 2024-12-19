8834ab1f5548e79269c7f64f276b3e4d824856a2
Srdn <giuseppespanedda@outlook.it>
2024-12-19 10:24:18 +0100

    Implementing Spring Security

--- src/main/java/com/pizzeria/PizzeriaApplication.java
+++ src/main/java/com/pizzeria/PizzeriaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.pizzeria.*")
 @SpringBootApplication
-@EntityScan(basePackages = "com.pizzeria.*")
 public class PizzeriaApplication {
 
 	public static void main(String[] args) {
